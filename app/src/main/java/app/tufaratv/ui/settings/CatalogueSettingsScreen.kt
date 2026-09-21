2d4c537567bbed0e64edf606961ea5b3a2560a19
tufara18-png <tufara18@gmail.com>
2026-09-21 10:03:35 -0400

    Fix catalogue settings layout import

--- app/src/main/java/app/tufaratv/ui/settings/CatalogueSettingsScreen.kt
+++ app/src/main/java/app/tufaratv/ui/settings/CatalogueSettingsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.LinearProgressIndicator
